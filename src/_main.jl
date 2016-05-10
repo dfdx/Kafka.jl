c6fc7bfb2757d588316aad7ac4d88c20025b2ab2
Andrei Zhabinski <faithlessfriend@gmail.com>
2016-05-11 00:52:40 +0300

    channel per result

--- src/_main.jl
+++ src/_main.jl
@@ -1,22 +1,10 @@
 
 include("core.jl")
 
-function main()
-    sock = connect("127.0.0.1", 9092)
-    header = RequestHeader(METADATA, 0, 5, "me")
-    req = TopicMetadataRequest(["test"])
-    send_request(sock, header, req)
-    header, resp = recv_response_with_header(sock, TopicMetadataResponse)
-end
-
-
 function main2()
     kc = KafkaClient("127.0.0.1", 9092; resp_loop=true)
-    ## metadata(kc, ["test"])
-    ## take!(kc.results)
-    ## kvs = [(b"1", b"hello"), (b"2", b"world")]
-    ## produce(kc, "test", 0, kvs)
-    ## take!(kc.results)
-    fetch(kc, "test", 0, 0)
-    r = take!(kc.results)
+    mch = metadata(kc, ["test"])    
+    kvs = [(b"1", b"hello"), (b"2", b"world")]
+    pch = produce(kc, "test", 0, kvs)
+    fch = fetch(kc, "test", 0, 0)
 end
--- src/client.jl
+++ src/client.jl
@@ -17,13 +17,13 @@ type KafkaClient
     sock::TCPSocket
     last_cor_id::Int64
     inprogress::Dict{Int64, Type}
-    results::Channel{Any}
+    results::Dict{Int64, Channel{Any}}
 end
 
 function KafkaClient(host::AbstractString, port::Int; resp_loop=true)
     sock = connect(host, port)
     inprogress = Dict{Int64, Type}()
-    results = Channel{Any}(10_000)
+    results = Dict{Int64, Channel{Any}}()
     kc = KafkaClient(sock, 0, inprogress, results)
     if resp_loop
         @async handle_responses(kc)
@@ -32,12 +32,15 @@ function KafkaClient(host::AbstractString, port::Int; resp_loop=true)
 end
 
 function handle_response(kc::KafkaClient)
-    # TODO: handle timed-out requests
-    header = recv_header(kc.sock)      
-    T = kc.inprogress[header.correlation_id]    
+    eof(kc.sock) # stop until there's new data
+    header = recv_header(kc.sock)
+    cor_id = header.correlation_id
+    T = kc.inprogress[cor_id]
     resp = readobj(kc.sock, T)    
-    delete!(kc.inprogress, header.correlation_id)
-    put!(kc.results, resp)
+    put!(kc.results[cor_id], resp)
+    delete!(kc.inprogress, cor_id)
+    delete!(kc.results, cor_id) # if nobody listens to a channel,
+                                #  GC should delete it as well
 end
 
 function handle_responses(kc::KafkaClient)
@@ -46,19 +49,25 @@ function handle_responses(kc::KafkaClient)
     end
 end
 
-function register_response_type{T}(kc::KafkaClient, ::Type{T})
+function register_request{T}(kc::KafkaClient, ::Type{T})
     kc.last_cor_id += 1
     id = kc.last_cor_id
     kc.inprogress[id] = T
-    return id
+    kc.results[id] = Channel(1)
+    return id, kc.results[id]
 end
 
+# metadata
+
 function metadata{S<:AbstractString}(kc::KafkaClient, topics::Vector{S})
-    cor_id = register_response_type(kc, TopicMetadataResponse)
+    cor_id, ch = register_request(kc, TopicMetadataResponse)    
     header = RequestHeader(METADATA, 0, cor_id, DEFAULT_ID)
     req = TopicMetadataRequest(topics)
     send_request(kc.sock, header, req)
+    return ch
 end
+
+# produce 
 
 # using Message version 0
 function make_message(key::Vector{UInt8}, value::Vector{UInt8})
@@ -80,9 +89,11 @@ function make_produce_request(topic::AbstractString, partition_id::Integer,
                               kvs::Vector{Tuple{Vector{UInt8}, Vector{UInt8}}})
     partition = Int32(partition_id)
     messages = [make_message(k, v) for (k, v) in kvs]
-    message_set = [MessageSetElement(0, obj_size(msg), msg) for msg in messages]
-    message_set_size = reduce(+, map(obj_size, message_set))
-    partition_data = PartitionData(partition, message_set_size, message_set)
+    message_set_elems = [MessageSetElement(0, obj_size(msg), msg)
+                         for msg in messages]
+    message_set_size = reduce(+, map(obj_size, message_set_elems))
+    partition_data = PartitionData(partition, message_set_size,
+                                   MessageSet(message_set_elems))
     topic_data = TopicData(topic, [partition_data])
     required_acks = 1 # broker will send response after writing to a local log
     timeout = 10_000  # in milliseconds
@@ -93,10 +104,11 @@ end
 function produce(kc::KafkaClient, topic::AbstractString, partition_id::Integer,
                  kvs::Vector{Tuple{Vector{UInt8}, Vector{UInt8}}})
     partition = Int32(partition_id)
-    cor_id = register_response_type(kc, ProduceResponse)
+    cor_id, ch = register_request(kc, ProduceResponse)
     header = RequestHeader(PRODUCE, 0, cor_id, DEFAULT_ID)
     req = make_produce_request(topic, partition, kvs)
     send_request(kc.sock, header, req)
+    return ch
 end
 
 function make_fetch_request(topic::AbstractString, partition_id::Integer,
@@ -111,8 +123,9 @@ end
 function Base.fetch(kc::KafkaClient, topic::AbstractString, partition_id::Integer,
                offset::Int64)
     partition = Int32(partition_id)
-    cor_id = register_response_type(kc, FetchResponse)
+    cor_id, ch = register_request(kc, FetchResponse)
     header = RequestHeader(FETCH, 0, cor_id, DEFAULT_ID)
     req = make_fetch_request(topic, partition, offset)
     send_request(kc.sock, header, req)
+    return ch
 end
